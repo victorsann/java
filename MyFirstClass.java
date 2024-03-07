63701f71dd21ff3f14fd9864aa9eeb9f61921be8
Victor Santos <jvic71174@gmail.com>
2024-03-07 12:09:07 -0300

    Java

--- MyFirstClass.java
+++ MyFirstClass.java
@@ -1,10 +1,10 @@
-package Java;
-
 public class MyFirstClass {
+
+    static String name = "Victor";
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println("Hello, " + name);
 
     }
 
